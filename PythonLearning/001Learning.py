61b205129ed05fc9a87b7cf5d4a834a643ea017a
zl2290103097 <2290103097@qq.com>
2017-08-24 16:08:05 +0800

    提交

--- PythonLearning/001Learning.py
+++ PythonLearning/001Learning.py
@@ -1,4 +1,4 @@
-#数据预处理 第四章
+#数据预处理 第四章 20170824
 
 import pandas as pd
 from io import StringIO
@@ -17,3 +17,11 @@ print(df.dropna(axis=1))  #删除至少缺失一个值的列
 print(df.dropna(how='all'))  #改行的所有数值缺失,则删除
 print(df.dropna(thresh=4))  #删除没有至少4个非空的行
 print(df.dropna(subset=['A'])) #删除该列缺失的行
+
+#缺失数据填充
+from sklearn.preprocessing import  Imputer
+imr = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imr = imr.fit(df)
+imputed_data = imr.transform(df.values)
+print(imputed_data)
+
